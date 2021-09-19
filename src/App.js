d0ece81f8d48b73562c916b4c9389ba984bbd1bc
Roshan Kr. Mahato <mahatoroshan1999@gmail.com>
2021-09-19 18:52:00 +0530

    created: first styled component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import styled from "styled-components";
+
+const MainTitle = styled.h1`
+  text-align: center;
+  text-transform: capitalize;
+  color: aliceblue;
+`;
 
 function App() {
-  return <h1 style={{ textAlign: "center", color: "green" }}>Styled Components</h1>
+  return (
+    <div>
+<MainTitle>styled components</MainTitle>
+    </div>
+  )
 }
 
 export default App;
